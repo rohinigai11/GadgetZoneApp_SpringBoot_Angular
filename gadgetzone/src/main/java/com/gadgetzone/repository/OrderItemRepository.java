85a4ca640e1882d19f2048f820a9f48ee9eee1e2
Rohini Gaikwad <rohinigaikwad2004@gmail.com>
2025-04-26 23:52:01 +0530

    Delete gadgetzone directory

--- gadgetzone/src/main/java/com/gadgetzone/repository/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gadgetzone.repository;
-
-import com.gadgetzone.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> { }
--- gadgetzone/src/main/java/com/gadgetzone/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.gadgetzone.service;
-
-import com.gadgetzone.entity.User;
-import java.util.Optional;
-
-public interface UserService {
-    User saveUser(User user);  // Method to save a user
-    Optional<User> getUserByEmail(String email);  // Method to get user by email
-}
